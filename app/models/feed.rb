f0f61a8120472a94ef57ace9a180e165842ab42d
Zhang Da <zhangda34ko@gmail.com>
2012-01-07 02:57:06 +0800

    filter

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,27 +1,66 @@
+require 'open-uri'
 class Feed < ActiveRecord::Base
 
   def self.update(url)
     feed = Feedzirra::Feed.fetch_and_parse(url)
-    add_entries(feed.entries)
+    keywords = []
+    add_entries(feed.entries, keywords)
   end
 
  
   private
     
-    def self.add_entries(entries)
-	entries.each do |entry|	
-	  unless exists? :guid => entry.id
-            create!(
-              :title => entry.title,
-              :url => entry.url,
-              :summary => entry.summary,
-              :content => entry.content,
-              :published_at => entry.published,
-              :guid => entry.id
-            )
+    def self.add_entries(entries, keywords)
+	entries.each do |entry|
+	  if should_add?(entry, keywords)
+            add_entry(entry)
           end
         end
     end	
+
+    def self.has_keyword?(content, keywords)
+       keywords.each do |keyword|
+         if content.downcase.include? keyword.downcase
+           return true
+         end
+       end
+       return false
+    end
+
+    def self.add_entry(entry)
+       create!(
+              :title => entry.title,
+              :url => entry.url,
+              :summary => entry.summary,
+              # :content => content,
+              :published_at => entry.published,
+              :guid => entry.id
+       )
+    end
+
+    def self.should_add?(entry, keywords)
+        if exists? :guid => entry.id
+          return false
+        end
+        if keywords.empty? 
+          return true
+        end
+        if has_keyword?(entry.summary, keywords)
+          return true
+        end
+        begin
+          doc = Nokogiri::HTML(open(entry.url).read)
+          doc.xpath('//p').each do |node|
+            if has_keyword?(node.text, keywords)
+              return true
+            end
+          end
+        rescue Exception => msg
+          puts msg
+        end
+        return false
+    end
+
 end
 
 # == Schema Information
