e7ba74623ae92e1fe0f4f58a0ef4f368d3c26078
Zhang Da <zhangda34ko@gmail.com>
2012-01-07 23:51:41 +0800

    done session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,12 +4,12 @@ module SessionsHelper
       :value => user.id,
       :expires => 1.hour.from_now
     }
-    self.current_user = user
+    current_user = user
   end
 
   def sign_out
     cookies.delete(:current_user)
-    self.current_user = nil
+    current_user = nil
   end
 
   def signed_in?
@@ -17,19 +17,36 @@ module SessionsHelper
   end
  
   def current_user
-     @current_user ||=cookies[:current_user]
+     if cookies[:current_user].nil?
+       return @current_user
+     else
+       @current_user ||=User.find(cookies[:current_user].to_i)
+     end
   end
    
   def current_user=(user)
      @current_user = user
   end
+ 
+  def authenticate
+    deny_access unless signed_in?
+  end
 
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page"
+  end
+
+ 
   def redirect_back_or(default)
-
     redirect_to(session[:return_to] || default)
     clear_return_to
   end
-
+  
+  def current_user?(user)
+    user == current_user
+  end
+ 
 
   private
  
