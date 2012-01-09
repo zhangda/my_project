d5a17d84e781a6f036802c8c1cb4f0bb8bea4375
Zhang Da <zhangda34ko@gmail.com>
2012-01-09 21:37:11 +0800

    Revert "done session"
    This reverts commit e7ba74623ae92e1fe0f4f58a0ef4f368d3c26078.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,4 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :except => [:new, :create, :show]
-  before_filter :correct_user, :only => [:edit, :update]
-  before_filter :admin_user, :only => [:index, :destroy]
-
   def show
     @user = User.find(params[:id])
   end
@@ -31,7 +27,7 @@ class UsersController < ApplicationController
       flash[:success] = "Profile updated!"
       redirect_to @user
     else
-      render 'edit'
+      redner 'edit'
     end
   end
 
@@ -44,17 +40,5 @@ class UsersController < ApplicationController
   def index
     @users = User.paginate(:page => params[:page])
   end
-
-  private
-
-    def correct_user
-       @user = User.find(params[:id])
-       redirect_to(root_path) unless current_user?(@user)
-    end
-
-    def admin_user
-       redirect_to(root_path) unless current_user.admin?
-    end
-
 
 end
